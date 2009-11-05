fdfec1fa45435433cb23a3962ee226aeb3854f86
SND\jimmyzimms_cp <SND\jimmyzimms_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2009-11-05 23:54:40 +0000

    git-svn-id: https://linqfu.svn.codeplex.com/svn@38432 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ Trunk/Source/LinqFu/ExpressionBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace LinqFu
+{
+    /// <summary>
+    /// The main entry point into the LinqFu system.
+    /// </summary>
+    public class ExpressionBuilder
+    {
+        /// <summary>
+        /// Converts the supplied <paramref name="expression"/> from <typeparamref name="TPrime"/> to <typeparamref name="TTheta"/>.
+        /// </summary>
+        /// <typeparam name="TPrime">The type to convert the expression from.</typeparam>
+        /// <typeparam name="TTheta">The type to convert the expression to.</typeparam>
+        /// <param name="expression">The <see cref="Expression{TDelegate}">expression</see> to convert.</param>
+        /// <returns>The converted function.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="expression"/> is null.</exception>
+        public static Expression<Func<TTheta, Boolean>> Convert<TPrime, TTheta>(Expression<Func<TPrime, Boolean>> expression) // where TTheta : TPrime
+        {
+            expression.
+            return null;
+        }
+    }
+}
