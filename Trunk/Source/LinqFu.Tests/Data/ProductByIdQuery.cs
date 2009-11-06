367afa4a60d35b5b0e5ea35413b2df044e55619a
SND\jimmyzimms_cp <SND\jimmyzimms_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2009-11-06 20:28:09 +0000

    added a basic query pattern object to assist in our test cases
    git-svn-id: https://linqfu.svn.codeplex.com/svn@38624 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ Trunk/Source/LinqFu.Tests/Data/ProductByIdQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqFu.Tests.Data
+{
+    public class ProductByIdQuery
+    {
+        private readonly IQueryable<Product> provider;
+
+        public ProductByIdQuery(IQueryable<Product> provider)
+        {
+            if (provider == null) throw new ArgumentNullException("provider");
+
+            this.provider = provider;
+        }
+
+        public IQueryable<Product> CreateQuery(Int32 productId)
+        {
+            var query = from p in this.provider
+                        where p.ProductID == 1
+                        select p;
+
+            return query;
+        }
+    }
+}
